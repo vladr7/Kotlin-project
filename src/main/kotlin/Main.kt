b98ce2b2033718ada328041e03517344e1ac7447
Vlad <vlad.ricean@brivo.com>
2022-08-11 18:35:31 +0300

    -lambda functions (functions inside parameters)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,13 @@
 fun main(args: Array<String>) {
 
-    val a = 3.0
-    val b = 4.0
-    val height = 2.0
-    val parallelogram = object : Shape("Parallelogram", a, b, height) {
-
-        init {
-            println("Paralul $a $b $height")
-        }
-
-        override fun area(): Double {
-            return a * b * height
-        }
-
-        override fun perimeter(): Double {
-            return a * b
-        }
-
-        fun isRectangle(): Boolean = height == b
-    }
+    star { sun() }
 }
 
+fun sun(): Boolean{
+    println("executed")
+    return true
+}
+
+fun star(function: () -> (Boolean)) {
+    println(function())
+}
